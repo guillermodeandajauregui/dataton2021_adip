388da57b6a9ac6eacbb0e9774a4bf25d71ff6665
gdj <gdeanda@inmegen.edu.mx>
2021-10-20 18:12:45 -0500

    poblaciones ageb urbana

---
+++ pop_ageb_urbana.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(archive)
+library(vroom)
+library(sf)
+url <-"https://inegi.org.mx/contenidos/programas/ccpv/2020/datosabiertos/ageb_manzana/ageb_mza_urbana_09_cpv2020_csv.zip"
+
+
+td<- tempdir()# Descarga del archivo temporaltf = tempfile(tmpdir=td, fileext=".zip")
+tf = tempfile(tmpdir=td, fileext=".zip")
+download.file(url, tf)# unzipunzip(tf, files="viviendas.csv", exdir=td, 
+mi_file <- archive::archive(tf) %>% pull(path) %>% head(2) %>% tail(1)
+x <- vroom::vroom(file = archive_read(archive = tf, file = mi_file))
+
+unlink(td)
+unlink(tf)
+rm(td, tf)
+
+pop.ageb <- 
+  x %>% 
+  #filter(!grepl("Total", NOM_LOC)) %>%
+  filter(grepl("Total AGEB", NOM_LOC)) %>% 
+  mutate(CVEGEO=paste0(ENTIDAD,MUN,LOC, AGEB)) %>% 
+  select(CVEGEO, POBTOT) %>% 
+  group_by(CVEGEO) %>% 
+  summarise(pop = sum(POBTOT))
+
+
+
+ageb_shp   <- sf::st_read(dsn = "data/marco_geoestadistico2020/conjunto_de_datos/00a.shp") %>% filter(CVE_ENT=="09") %>% st_transform(crs = 4269) 
+ageb_shp$CVEGEO %>% head() %>% str_length()
+
+vroom::vroom_write(x = pop.ageb, file = "intermediates/pop_ageb_urbana.txt")
